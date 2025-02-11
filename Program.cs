8b1e0b5586c4756b2fe4813859f6bf0e6f63fa44
bb1011659 <Bb1011659@otc.edu>
2025-02-11 10:09:45 -0600

    Catch block assignment

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace CatchBlocks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "49"; // or use "49" in a notebook
+            try
+            {
+                int age = int.Parse(input);
+                Console.WriteLine($"You are {age} years old.");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("The age you entered is not a valid number format.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex.GetType()} says {ex.Message}");
+            }
+        }
+    }
+}
